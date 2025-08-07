291547f79ef6d12116df3a3d43351d95a18ebfa2
godxrs <67811850+godxrs@users.noreply.github.com>
2025-08-07 16:09:18 -0500

    Deleting some

--- Backend/src/routes/inspectionsRoute.js
+++ Backend/src/routes/inspectionsRoute.js
@@ -1,20 +1,3 @@
-// import express from "express";
-
-// import { createTransaction, deleteTransaction, getTransactionsByUserId, getSummaryByUser, getSingleTransaction } from "../controllers/transactionsController.js";
-
-// const router = express.Router();
-
-// router.post("/", createTransaction);
-
-// router.get("/single/:id", getSingleTransaction);
-
-// router.get("/:userId", getTransactionsByUserId);
-
-// router.delete("/:id", deleteTransaction);
-
-// router.get("/summary/:userId", getSummaryByUser);
-
-// export default router;
 import express from "express";
 import { 
     createInspection, 
